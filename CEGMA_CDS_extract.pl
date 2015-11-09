d3da74bc055977480e5de07e9682ee603feb55a8
Felix Grewe <fgrewe@fieldmuseum.org>
2015-11-09 14:01:07 -0600

    Upload of data

---
+++ CEGMA_CDS_extract.pl
@@ -0,0 +1,445 @@
+#!/usr/bin/perl -w
+#
+# CEGMA_CDS_extract.pl
+#
+# Discription  :	Maps NGS reads to CEGMA gene regions and extracts CDS for phylogenetic analyses.
+#
+# OS prog req  :    bowtie2, samtools, bam2consensus, muscle, raxmlHPC
+#
+# files req    :    output.cegma.dna, csv table
+# 
+# Author       :	Felix Grewe (fgrewe@fieldmuseum.org), www.felixgrewe.de
+# Date created :	05/29/2015
+# Last modified:	11/03/2015
+#
+# Modifications:    07/23/2015: paired-end reads for lichens; added samtoolsDepth, changed mpileup -A, switch for single end, changed trimmomatic parameters
+#					11/03/2015: soft coding, only used bambam, no trimmomatic, no depth, no mpileup
+
+use strict;
+
+use File::Which;
+use Getopt::Long;
+use Pod::Usage;
+use Bio::SimpleAlign;
+use Bio::AlignIO;
+use Bio::SeqIO;
+use Bio::Align::Utilities qw(:all);
+use Bio::Seq;
+
+##################
+# Get input data #
+##################
+
+### setup my defaults
+our ($fasta, $gff, $table, $reference, $cores, $run_phylo, $help);
+$fasta = "output.cegma.dna";
+$gff = "output.cegma.local.gff";
+$reference = "Ref";
+$run_phylo = undef;
+$cores = 8;
+
+GetOptions(	'fasta|f=s' 	=> \$fasta,
+			'gff|g=s'		=> \$gff,
+			'table|t=s'		=> \$table,
+			'reference|r=s'	=> \$reference,
+			'threads|p=i'	=> \$cores,
+			'run_phylo|y'	=> \$run_phylo,
+			'help|h|?'		=> \$help,
+);
+
+pod2usage(-verbose=>2) if $help;
+pod2usage(-verbose=>1) unless $table;
+
+# if (@ARGV == 5) {
+    # print "Starting program...\n\n";  
+# } else {
+    # print STDERR "usage: $0 <output.cegma.dna> <output.cegma.local.gff> <table> <Reference_name> <no_of_cores>\n";
+    # print STDERR "Please supply the output.cegma.dna file, output.cegma.local.gff, tab seperated table with sequencing information (taxon_name, absolute_path_to_forward_fastq_sequence_location, [absolute_path_to_reverse_fastq_sequence_location]), taxon_name of Reference sequence, and number of how many core you want to use!\n";
+    # exit; 
+# }
+
+# my $fasta = shift(@ARGV);
+# my $gff = shift(@ARGV);
+# my $table = shift(@ARGV);
+# my $reference = shift(@ARGV);
+# my $cores = shift(@ARGV);
+
+######################
+# Check dependencies #
+######################
+
+which("bowtie2") or die "Seems like bowtie2 is not installed.\n";
+which("samtools") or die "Seems like samtools is not installed.\n";
+which("bam2consensus") or die "Seems like bam2consensus from bambam package is not installed.\n";
+which("muscle") or die "Seems like muscle is not installed.\n";
+if (defined($run_phylo)) {
+	which("raxmlHPC") or die "Seems like raxmlHPC is not installed.\n";
+}
+#############################
+# Load sequence information #
+#############################
+
+### Reads reference cegma fasta
+
+my @ref_genes = ();
+my $ref = 0;
+my $id = 0;
+my %seq = ();
+
+open (INPUT_CEGMA, "$fasta") or die "Can't open cegma_fasta with reference reads!!!\n";
+
+while (my $line = readline(INPUT_CEGMA)) { # reads the fasta
+    chomp ($line);
+    if ($line =~ m/^>/) {      
+        #$fasta_name[$seq_cnt++] = $line; # reads fasta name and creats array - sugg by JJR      
+        $id = $line; # reads fasta and creates hash - sugg by JJR
+        $seq{$id} = ""; # empties values for key   
+    } else {     
+        #$fasta_seq[$seq_cnt] .= $line; # reads fasta sequence and fills array - sugg by JJ   
+        $seq{$id} .= $line; # reads fasta sequence and fills hash - sugg by JJR
+    }
+}
+
+close INPUT_CEGMA or die "Can't close cegma_fasta!!!\n";
+
+### Sequence location
+
+my @taxon = ();
+my @pe_path = ();
+my @pe_path2 = ();
+
+open (INPUT_TABLE, "$table") or die "Can't open table with sequencing information!!!\n";
+
+while (my $line = readline(INPUT_TABLE)) {
+    chomp $line;
+    
+    if ($line =~ m/^#/) { # doesn't read comments in the table               
+        next;       
+    } else {        
+        #writing into arrays
+        my @table = split ",", $line; 	# splitting the blast output and reading in arrays
+		push (@taxon, $table[0]);			# name of taxon
+        push (@pe_path, $table[1]);    		# absolute path to sequence location
+		push (@pe_path2, $table[2]);    	# absolute path to other sequence location
+    }
+}  
+
+close INPUT_TABLE or die "Can't close table!!!\n";
+
+
+### CEGMA annotation
+
+my @cegma_scaffold = ();
+my @cegma_exon_pos = ();
+my @cegma_start = ();
+my @cegma_stop = ();
+my @cegma_gene = ();
+
+my @cegma_aln_start = ();
+my @cegma_aln_stop = ();
+
+my %fas_loc = ();
+
+open (INPUT_GFF, "$gff") or die "Can't open cegma gff file!!!\n";
+while (my $line = readline(INPUT_GFF)) {
+    chomp $line;
+    
+    if ($line =~ m/^#/) { # doesn't read comments in the table               
+        next;       
+    } else {        
+        #writing into arrays
+        my @table = split (/\t/, $line); 
+#        push (@cegma_scaffold, $table[0]);
+		push (@cegma_exon_pos, $table[2]);
+		push (@cegma_start, $table[3]);
+		push (@cegma_stop, $table[4]);
+		push (@cegma_gene, $table[0]);
+    }
+}  
+
+close INPUT_GFF or die "Can't close gff!!!\n";
+
+### output Folder
+my $ali_folder = "OUTPUT_alignments";
+mkdir $ali_folder;
+my $CDS_folder = "OUTPUT_good_CDS_alignments";
+mkdir $CDS_folder;
+
+##################
+# Bowtie mapping #
+##################
+
+for (my $i = 0; $i < @taxon; $i++) {
+	my $fastq_reads;
+	
+	mkdir $taxon[$i];
+	chdir ($taxon[$i]) or die "Can't change into $taxon[$i]";
+		
+	if (exists($pe_path2[$i])){
+		$fastq_reads = $pe_path[$i].",".$pe_path2[$i];
+		print "Found multiple sequence files for: $taxon[$i]\n";
+	} else {
+		$fastq_reads = $pe_path[$i];
+		print "Found single sequence files for: $taxon[$i]\n";
+	}
+
+	print "###Start Bowtie2 for $taxon[$i]\n\n";
+	
+	my $bowtieBuild = 'bowtie2-build ../'.$fasta.' '.$taxon[$i];
+	my $bowtie2 = 'bowtie2 -x '.$taxon[$i].' -U '.$fastq_reads.' -N 1 -S '.$taxon[$i].'.sam -p '.$cores;
+	my $samtoolsView = 'samtools view -bS '.$taxon[$i].'.sam > '.$taxon[$i].'.bam';
+	my $samtoolsSort = 'samtools sort '.$taxon[$i].'.bam '.$taxon[$i].'.sorted';
+	my $samtoolsDepth = 'samtools depth -a -a '.$taxon[$i].'.sorted.bam > '.$taxon[$i].'.depth';
+	my $samtoolsMpileup = 'samtools mpileup -A -Q 0 -f ../'.$fasta.' '.$taxon[$i].'.sorted.bam > '.$taxon[$i].'.mpileup';
+	my $samtoolsIndex = 'samtools index '.$taxon[$i].'.sorted.bam';
+	my $samtoolsMpileupUF = 'samtools mpileup -uf '.$taxon[$i].'.fasta '.$taxon[$i].'.sorted.bam | bcftools call -c | vcfutils.pl vcf2fq > '.$taxon[$i].'_vcfcons.fastq';
+	my $fastq2fasta = 'awk \'/^@/,/^+$/\' '.$taxon[$i].'_vcfcons.fastq | perl -pe "s/@/>/ ; s/\\+//" > '.$taxon[$i].'_vcfcons.fasta'; 
+	my $bam2consensus = 'bam2consensus -i 0.1 '.$taxon[$i].'.sorted.bam > '.$taxon[$i].'_bambamcons.fas';
+	my $copy = 'cp '.$taxon[$i].'_bambamcons.fas ../'.$ali_folder;
+	
+	system ($bowtieBuild);
+	system ($bowtie2);
+	system ($samtoolsView);
+	system ($samtoolsSort);
+#	system ($samtoolsDepth);
+	system ($samtoolsMpileup);
+	system ($samtoolsIndex);
+#	system ($samtoolsMpileupUF);
+#	system ($fastq2fasta);
+	system ($bam2consensus);
+	system ($copy);
+	
+	print "###Bowtie2 for $taxon[$i] finished\n\n";
+	
+	chdir ("..");
+}
+
+###################
+# Build alignment #
+###################
+
+chdir ($ali_folder) or die "Can't change into $ali_folder";
+
+print "Building alignments\n\n";
+
+foreach my $key (sort(keys %seq)) {
+	my ($gene_name) = $key =~ m/^\>(.*)/;
+#	print "Gene name:\n";
+#	print $gene_name."\n";
+	open (OUTPUT_FASTA, ">".$gene_name."_region.fas") or die "Can't open output fasta file!!!\n";
+	print OUTPUT_FASTA ">$reference\n";
+	print OUTPUT_FASTA $seq{$key}."\n";
+	close OUTPUT_FASTA or die "Can't close output fasta from reference round!!!\n";
+}
+
+for (my $i = 0; $i < @taxon; $i++) {
+
+	my $mapping_id = 0;
+	my %mapping_seq = ();
+
+	open (MAPPING_FASTA, "$taxon[$i]_bambamcons.fas") or die "Can't open mapping fasta file for $taxon[$i]!!!\n";
+	
+	while (my $line = readline(MAPPING_FASTA)) {
+		chomp ($line);
+		if ($line =~ m/^>/) {       
+			$mapping_id = $line;
+			$mapping_seq{$mapping_id} = "";
+		} else {     
+			$mapping_seq{$mapping_id} .= $line;
+		}
+	}
+	
+	close MAPPING_FASTA or die "Can't close mapping fasta file!!!\n";
+	
+	foreach my $mapping_key (sort(keys %mapping_seq)) {
+		my ($gene_name) = $mapping_key =~ m/^\>(.*)/;
+		
+		open (OUTPUT_FASTA, ">>".$gene_name."_region.fas") or die "Can't open output fasta file!!!\n";
+		print OUTPUT_FASTA ">$taxon[$i]\n";
+		print OUTPUT_FASTA $mapping_seq{$mapping_key}."\n";
+		close OUTPUT_FASTA or die "Can't close output fasta from mapping round!!!\n";
+	}
+}
+
+print "All alignmtes are done\n\n";
+
+########################################
+# run muscle on all CEGAM region files #
+########################################
+
+foreach my $key (sort(keys %seq)) {
+	my ($gene_name) = $key =~ m/^\>(.*)/;
+	my $file_name = $gene_name."_region.fas";
+	my $file_out = $gene_name."_regions_muscle.fas";
+	my $muscle = 'muscle -in '.$file_name.' -out '.$file_out;
+	
+	system ($muscle);
+}
+	
+# #####################################################
+# # Build RaxML phylogenies on all CEGMA region files #
+# #####################################################
+
+# if (!defined($no_phylo)) {
+
+	# foreach my $key (sort(keys %seq)) {
+		# my ($gene_name) = $key =~ m/^\>(.*)/;
+		# my $alignment = $gene_name."_regions_muscle.fas";
+		# my $file_out = $gene_name."_regions_muscle.phy";
+		# my $rax_out = $gene_name."_regions_RAXML";
+		
+		# my $in = Bio::AlignIO->new(-file => $alignment, -format => "fasta");
+		# my $out = Bio::AlignIO->new(-file => ">".$file_out, -format => "phylip");
+		
+		# while (my $aln = $in = $in->next_aln) {
+			# $out->write_aln($aln);
+		# }
+		
+		# my $raxml = 'raxmlHPC -s '.$file_out.' -n '.$rax_out.' -m GTRGAMMA -f a -p 194955 -x 12345 -# 100 -T '.$cores;
+		
+		# system ($raxml);
+	# }
+# }
+
+########################
+# Alignment processing #
+########################
+
+my %sliced_alis = ();
+my %combined_alis = ();
+my %sliced_no = ();
+my $current_gene = $cegma_gene[0];
+
+for (my $i = 0; $i < @cegma_gene; $i++) {
+	my $to_open = $cegma_gene[$i]."_regions_muscle.fas";
+	my $str = Bio::AlignIO->new(-file => $to_open, -format => "fasta");
+	my $aln = $str->next_aln();
+	my $pos_start = $aln->column_from_residue_number($reference, $cegma_start[$i]);
+	push (@cegma_aln_start, $pos_start); 			#saving new start position in array
+	my $pos_stop = $aln->column_from_residue_number($reference, $cegma_stop[$i]);
+	push (@cegma_aln_stop, $pos_stop); 				#saving new stop positions in array
+	my $sliced = $aln->slice($pos_start, $pos_stop, 1);
+	$sliced_no{$cegma_gene[$i]}++;
+	$sliced_alis{$cegma_gene[$i]}{$sliced_no{$cegma_gene[$i]}} = $sliced;
+	$combined_alis{$cegma_gene[$i]} = "";
+}
+
+my $ali123 = 0;
+my $Ref_seq = 0;
+my $Ref_prot = 0;
+
+foreach my $gene (sort keys %sliced_alis) {
+
+	my $count = 0;
+	foreach my $number (sort keys %{ $sliced_alis{$gene} }) {
+		if ($number == 1) {
+			$ali123 = $sliced_alis{$gene}{$number};
+		} elsif ($number > 1) {
+			$ali123 = cat($ali123, $sliced_alis{$gene}{$number});
+		}
+	}
+	
+	### Getting rid of all other insertions in all other sequences
+	my $ali123_ref = $ali123->set_new_reference($reference);
+	my $ali123_nogap = $ali123_ref->remove_gaps;
+	$ali123_nogap->set_displayname_flat();
+	
+	### Check if alignment is complete by ORF
+	foreach my $seq_obj ($ali123_nogap->each_seq_with_id($reference)) {
+		$Ref_seq = $seq_obj->seq();
+		$Ref_prot = ($seq_obj->translate)->seq();
+	}
+	$count = ($Ref_prot =~ tr/\*//);
+	
+	if ($Ref_seq !~ /^ATG/) {
+		print "$gene does not start with ATG and was excluded from further analysis\n";
+	} elsif ($Ref_seq !~ /TGA$|TAA$|TAG$/) {
+		print "$gene does not stop with TGA, TAA, or TAG and was excluded from further analysis\n"; 
+	} elsif ($count > 1) {
+		print "$gene has $count stop codons in translation and was excluded from further analysis\n";
+	} elsif ((length($Ref_seq)%3) != 0) {
+		print "$gene is not divisible by 3 and was excluded from further analysis\n";
+	} else {
+		my $to_close_fas = "../".$CDS_folder."/".$gene."_CDS.fas";
+		my $out_fas = Bio::AlignIO->new(-file => ">".$to_close_fas, -format => "fasta");
+		$out_fas->write_aln($ali123_nogap); 
+
+		my $to_close_phy = "../".$CDS_folder."/".$gene."_CDS.phy";
+		my $out_phy = Bio::AlignIO->new(-file => ">".$to_close_phy, -format => "phylip");		
+		$out_phy->write_aln($ali123_nogap); 
+	}
+}
+chdir ("..");
+
+############################################
+# Build RaxML phylogenies on all CEGMA CDS #
+############################################
+
+if (defined($run_phylo)) {
+	my $phy_folder = "OUTPUT_phylogenies";
+	mkdir $phy_folder;
+	chdir ($phy_folder) or die "Can't change into $phy_folder";
+	
+	my $move = 'mv ../'.$CDS_folder.'/*.CDS.phy .';
+	system ($move);
+
+	foreach my $gene_name (sort(keys %sliced_alis)) {
+		
+		my $file_out = $gene_name."_CDS.phy";
+		my $rax_out = $gene_name."_CDS_RAXML";
+
+		my $raxml = 'raxmlHPC -s '.$file_out.' -n '.$rax_out.' -m GTRGAMMA -f a -p 194955 -x 12345 -# 100 -T '.$cores;
+		
+		system ($raxml);
+	}
+	chdir ("..");
+} else {
+	my $del = 'rm ../'.$CDS_folder.'/*.CDS.phy .';
+	system ($del);
+}
+
+print "All done. Enjoy!\n";
+
+
+__END__
+
+=head1 NAME
+
+CEGMA_CDS_extract
+
+=head1 COPYRIGHT
+
+   copyright (C) 2015 Felix Grewe
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 DESCRIPTION
+
+   Maps NGS reads to CEGMA gene regions and extracts CDS for phylogenetic analyses.
+
+=head1 SYNOPSIS
+
+CEGMA_CDS_extract.pl  -t [-f -g -r -p -y -h]
+
+ -table -t <.csv>		Comma seperated table with NGS sequence read location (name,se_or_pe1_reads[,se_or_pe2_reads])
+ 
+ [-fasta -f <.fasta>]		reference sequence file (default=output.cegma.dna)
+ [-gff -g <.gff>]		annotation of reference sequence file (default=output.cegma.local.gff)
+ [-reference -r <STRING>]	Name of reference sequence (default=Ref)
+ [-threads -p <INT>]		Number of available phreads (default=8)
+ [-run_phylo -y]		If you want to run RAxML phylogeny for individual genes
+ [-help -h]			Displays this basic usage information 
+ 
+
+
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# CEGMA_CSD_extract
-Maps NGS reads to CEGMA gene regions and extracts CDS for phylogenetic analyses
+#CEGMA_CDS_extract
+
+A script that maps similar reads to a reference sequence file and extracts genes by annotation (such as the [CEGMA](http://korflab.ucdavis.edu/datasets/cegma/) output) with the advantage that each individual gene remains in frame with the genetic code. This allows for subsequent analyses for example to split the data by codon position or to select for individual genes. Although we used and tested the program specificlly for the CEGMA output each gff annotated and reference sequence should work.
+
+The script offers an option to build a RAxML phylogeny of each individual alignment. However, individual alignments can also be merged into a supermatrix for further analysis.
+
+If you use this code please cite:  
+Steven D. Leavitt, Felix Grewe, Todd Widhelm, Lucia Muggia, Brian Wray, and H. Thorsten Lumbsch **Robust phylogenies of lichen-forming fungi inferred from genomes - overkill in the era of phylogenomics or reassurance?** *In prep*  
+
+##Requirements:
+
+Installed globally  
+- Perl (with BioPerl)  
+- bowtie2  
+- samtools   
+- bam2consensus (from the bambam package)  
+- muscle  
+- raxmlHPC (optional)  
+
+-------------------------
+
+##To run the script:
+
+###Illumina sequence data
+You can use either single-end or paired-end reads in this analysis but all reads will be handled as single-ended. We recommend to trim your reads before use; e.g. with [Trimmomatic](http://www.usadellab.org/cms/?page=trimmomatic).  
+
+###Have you files ready  
+You need several files to get going:  
+1. A comma separated table that points to your sequence read files (see [seq_loc.csv](https://github.com/felixgrewe/CEGMA_CDS_extract/blob/master/seq_loc.csv) as an example)  
+2. Reference fasta file; we used the CEGMA standard output file *output.cegma.dna*  
+3. Annotation of the reference fasta file (see 2) in gff format; we used the CEGMA standard output file *output.cegma.local.gff*  
+
+###Run the script 
+~~~
+CEGMA_CDS_extract.pl  -t [-f -g -r -p -y -h]
+
+ -table -t <.csv>			Comma seperated table with NGS sequence read location (name,se_or_pe1_reads[,se_or_pe2_reads])
+ 
+ [-fasta -f <.fasta>]		reference sequence file (default=output.cegma.dna)
+ [-gff -g <.gff>]			annotation of reference sequence file (default=output.cegma.local.gff)
+ [-reference -r <STRING>]	Name of reference sequence (default=Ref)
+ [-threads -p <INT>]		Number of available phreads (default=8)
+ [-run_phylo -y]			If you want to run raxML phylogeny for individual genes
+ [-help -h]					Displays this basic usage information 
+~~~
+
+>Please note that you chose if you want to run RAxML analyses for each individual gene alignment.
+
+###Output
+The script will produce two output folders. The *OUTPUT_alignments* folder holds all alignments of mapping across whole fasta reference sequences and the *OUTPUT_good_CDS_alignments* folder contains all good gene alignments which are in frame to the open reading frame of the reference annotation. 
+If you chose to analyze phylogenetic inferences for each individual good gene alignment (option -y), you'll also see a *OUTPUT_phylogenies* folder that contains all RAxML trees.
+
+###Further Processing
+
+####Supermatrix
+You can use the program [FASconCAT](https://www.zfmk.de/en/research/research-centres-and-groups/fasconcat) (Kueck & Meusemann, 2010) to build a supermatrix. Place the FASconCAT perl scrip into the *OUTPUT_good_CDS_alignment* folder and execute with
+~~~
+perl FASconCAT_v1.0.pl
+~~~
+and follow the instructions.
+
+####Combining all trees
+If you chose the -f option before and see the *OUTPUT_phylogenies* folder, you can combine all individual gene trees into one newick file with:  
+~~~
+cat ./OUTPUT_phylogenies/*\.bipartitions\.*RAXML .
+~~~
+
